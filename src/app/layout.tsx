9c2d92285c2dbe7aba0dc4b69c362d95b5f8b2b5
Akshay Satish <akkusat1987@gmail.com>
2024-01-12 14:57:13 -0500

    Favicon Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Akshay Satish',
   description: 'My Personal Website!',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        href: '/favicon.ico',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
@@ -19,6 +27,7 @@ export default function RootLayout({
 
   return (
       <html lang="en">
+
         <body className={inter.className + " bg-zinc-100 dark:bg-zinc-900 ease-in-out duration-150 space-y-4"}>
         
           <Navbar/>
